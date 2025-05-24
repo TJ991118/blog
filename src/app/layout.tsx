b91bf63a6d4d1d4db91aed70189a7ad921b9049e
Weiren Gan <weirengan@WeirendeMacBook-Pro.local>
2025-05-24 14:47:37 +0800

    ✨feat(config): setup theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
+import "./globals.scss";
+import "@/styles/theme/theme.scss";
+
+import ThemeProvider from "@/common/theme/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );
